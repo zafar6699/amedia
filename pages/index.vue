764a9b93b1e6b9f58f594c9cb4345f74f39838e9
Zafar6699 <webzafar6699@gmail.com>
2021-11-27 18:21:11 +0500

    ad

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,10 @@
 </template>
 
 <script>
-import News from '../components/home/News.vue'
-import Animes from '../components/home/Animes.vue'
-import Slider from '../components/home/Slider12.vue'
-import Trailer from '../components/home/Trailer1.vue'
+import News from '@/components/home/News.vue'
+import Animes from '@/components/home/Animes.vue'
+import Slider from '@/components/home/Slider12.vue'
+import Trailer from '@/components/home/Trailer1.vue'
 
 export default {
     components: {
